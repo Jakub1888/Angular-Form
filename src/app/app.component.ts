b014f74ebf44c77816bc3660b6768d95ed2d9c93
Jakub1888 <krasnan.jk@gmail.com>
2021-03-22 06:26:07 +0100

    Fom update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tdf';
+  submitted = false;
+  step = 1;
+
+  userModel = new User();
+
+  constructor(private _enrollmentService: EnrollmentService) {}
+
+  onSubmit() {
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel).subscribe(
+      (data) => console.log('success', data),
+      (error) => console.log('error', error)
+    );
+  }
+
+  nextStep() {
+    this.step += 1;
+  }
+
+  previousStep() {
+    this.step -= 1;
+  }
+  
 }
